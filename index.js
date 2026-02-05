942965ae720f8ec559d8e862de36c2de00e85a67
IDGS-904-22001661 <armandodanielr29@outlook.com>
2026-02-05 13:47:15 -0600

    conexiones externas

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ app.use('/api', timeRoutes);
 app.use('/api',asistenciaRoutes)
 
 // Servidor
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+// QUE ACEPTE CONEXIONES EXTERNAS
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
 });
