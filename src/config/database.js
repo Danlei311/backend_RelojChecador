48cf66ac449388282edfc4f89ca51f4fce2f698f
IDGS-904-22001661 <armandodanielr29@outlook.com>
2026-02-09 11:06:12 -0600

    test(conexion): verificacion de conexion al servidor

--- src/config/database.js
+++ src/config/database.js
@@ -3,12 +3,22 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+console.log("🔍 VARIABLES DE ENTORNO QUE ESTÁ RECIBIENDO NODE:");
+console.log({
+  DB_HOST: process.env.DB_HOST,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_NAME: process.env.DB_NAME,
+  DB_PORT: process.env.DB_PORT
+});
+
+
 export const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    port: Number(process.env.DB_PORT),
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
